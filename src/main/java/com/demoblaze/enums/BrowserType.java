72828b1fa5852f235670a1867193f2303641aa73
GopiKishanBehera9 <Gopikishan9.official@gmail.com>
2026-07-25 11:16:21 +0530

    feat: add browser type enum

---
+++ src/main/java/com/demoblaze/enums/BrowserType.java
@@ -0,0 +1,14 @@
+package com.demoblaze.enums;
+
+/**
+ * Supported browsers for the framework.
+ *
+ * @author Gopi Kishan Behera
+ */
+public enum BrowserType {
+
+    CHROME,
+    EDGE,
+    FIREFOX
+
+}
